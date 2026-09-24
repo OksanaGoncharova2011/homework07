1e23dcdd69bde62b2658fe8e1a865957af8647e8
Oksana Goncharova <oksanagoncharova2011@gmail.com>
2026-09-24 21:39:19 +0800

    0000

--- src/Main.java
+++ src/Main.java
@@ -15,47 +15,38 @@ void main() {
 
     //task2
     System.out.println( "task 2" );
-    int userWantsToСontinue = 1;
-// Переменная, обозначающая, что юзер хочет продолжить игру
-// 1 — хочет продолжить, 0 — не хочет продолжать играть
-    int level = 1;
-// Переменная со значением номера уровня
+
+    int step = 500;
+    int distens = 42195;
+// Первая версия: цикл do-while
+    int distansCovered = 0;
+    System.out.println("Версия с циклом do-while:");
     do {
-        System.out.println("Завершен уровень" + level);
-        // Выводим текст с номером уровня игры
-        System.out.println("Хотите продолжить?");
-        // Спрашиваем, хочет ли пользователь продолжить игру
-        level++;
-        // К переменной level прибавляем 1, это следующий уровень
+        int distanceLeft = distens - distansCovered;
+        System.out.println( "Держитесь! Осталось " + distanceLeft + " метров" );
+          distansCovered = distansCovered += step;
+    } while (distansCovered < distens);
+
+
+
+    // Вторая версия: цикл for
+    System.out.println("Версия с циклом for:");
+
+    for (distansCovered = 0;
+         distansCovered < distens;
+         distansCovered += step) {
+
+        int distanceLeft = distens - distansCovered;
+
+        System.out.println(
+                "Держитесь! Осталось " + distanceLeft + " метров"
+        );
     }
-    while (userWantsToСontinue == 1 && level < 5);
-// Записываем условие: если пользователь хочет продолжить
-// и номер уровня меньше или равен 5
+
+
 
     //task3
-    int points = 5;
+    System.out.println( "task 3" );
 
-    do {
-        System.out.println("Осталось " + points + " баллов.");
-        points--;
-    } while (points >= 0);
 
-    System.out.println("Игра окончена!");
-
-    //task4
-    int salary = 65535;
-    int total = 0;
-    int i = 0;
-    for (; total < 1_000_000; i++) {
-        total = total + total/100;
-        total = total + salary;
-        if (i % 5 == 0) {
-            System.out.println("Месяц " + i + " Итого " + total);
-        }
-        if (i == 12) {
-            System.out.println("Окончание срока действия годового вклада!");
-            break;
-        }
-    }
-    System.out.println(total);
 }
